4e5afc1f75d3a2a12c233c623bcb4159c9fd2e85
Amogh N Rao <amogh090502@gmail.com>
2022-11-11 23:28:33 +0530

    created reminder and counter app in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counter';
+  title = 'Remainder App';
+  isLoggedin = false;
+  
+  login() {
+    this.isLoggedin = true;
+  }
+
+  logout() {
+    this.isLoggedin = false;
+  }
+  counter1 = 0;
+  counter(type: string) {
+    if (type == 'add') {
+      this.counter1++;
+    }
+    if (type == 'sub') {
+      this.counter1--;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RemainderComponent } from './remainder/remainder.component';
+import { RemainderListComponent } from './remainder-list/remainder-list.component';
+import { AddRemainderComponent } from './add-remainder/add-remainder.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RemainderComponent,
+    RemainderListComponent,
+    AddRemainderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
